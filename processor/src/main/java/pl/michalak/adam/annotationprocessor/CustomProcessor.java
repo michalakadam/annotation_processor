9a3523443d1a2abe8b05d573f201d1f47a7346f8
Adam Michalak <adam_michalak@epam.com>
2019-02-18 09:43:52 +0100

    processor is here, but no output available

--- processor/src/main/java/pl/michalak/adam/annotationprocessor/CustomProcessor.java
+++ processor/src/main/java/pl/michalak/adam/annotationprocessor/CustomProcessor.java
@@ -1,4 +1,32 @@
 package pl.michalak.adam.annotationprocessor;
 
-class CustomProcessor {
+import pl.michalak.adam.annotations.Run;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.TypeElement;
+import javax.tools.Diagnostic;
+import java.util.Set;
+
+
+@SupportedAnnotationTypes("pl.michalak.adam.annotations.Run")
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
+public class CustomProcessor extends AbstractProcessor {
+
+    public CustomProcessor(){
+        super();
+    }
+
+    @Override
+    public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
+        for(Element element : roundEnv.getElementsAnnotatedWith(Run.class)) {
+            String message = "annotation @Run found in " + element.getSimpleName();
+            processingEnv.getMessager().printMessage(Diagnostic.Kind.NOTE, message);
+        }
+        return true;
+    }
 }
